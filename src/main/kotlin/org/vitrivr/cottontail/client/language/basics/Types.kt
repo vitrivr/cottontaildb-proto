4ef37e7625f298278871100b49224557faf649d5
Ralph Gasser <rg@pontius.ch>
2021-02-19 10:28:24 +0100

    A enumeration of all types available to Cottontail DB.

---
+++ src/main/kotlin/org/vitrivr/cottontail/client/language/basics/Types.kt
@@ -0,0 +1,29 @@
+package org.vitrivr.cottontail.client.language.basics
+
+
+/**
+ * The [Types] available in the Cottontail DB type system.
+ *
+ * @author Ralph Gasser
+ * @version 1.0.0
+ */
+enum class Types {
+    BOOLEAN,
+    BYTE,
+    SHORT,
+    INT,
+    LONG,
+    DATE,
+    FLOAT,
+    DOUBLE,
+    STRING,
+    COMPLEX32,
+    COMPLEX64,
+    INT_VEC,
+    LONG_VEC,
+    FLOAT_VEC,
+    DOUBLE_VEC,
+    BOOL_VEC,
+    COMPLEX32_VEC,
+    COMPLEX64_VEC;
+}
