a55b34c99380a05bf227a3f4a7ddade092ca2f90
Ralph Gasser <rg@pontius.ch>
2021-10-12 18:21:54 +0200

    Removed reference to raw tuple CottontailGrpc.QueryResponseMessage.Tuple

--- src/main/kotlin/org/vitrivr/cottontail/client/iterators/Tuple.kt
+++ src/main/kotlin/org/vitrivr/cottontail/client/iterators/Tuple.kt
@@ -9,10 +9,10 @@ import java.util.*
  * @author Ralph Gasser
  * @version 1.1.1
  */
-abstract class Tuple(val raw: CottontailGrpc.QueryResponseMessage.Tuple) {
+abstract class Tuple(tuple: CottontailGrpc.QueryResponseMessage.Tuple) {
     /** Internal list of values. */
-    private val values: Array<Any?> = Array(raw.dataCount) { it ->
-        val data = raw.dataList[it]
+    private val values: Array<Any?> = Array(tuple.dataCount) { it ->
+        val data = tuple.dataList[it]
         when (data.dataCase) {
             CottontailGrpc.Literal.DataCase.BOOLEANDATA -> data.booleanData
             CottontailGrpc.Literal.DataCase.INTDATA -> data.intData
