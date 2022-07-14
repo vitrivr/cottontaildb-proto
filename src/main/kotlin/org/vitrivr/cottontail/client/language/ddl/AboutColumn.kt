4ae89c0d79b9c8d85f895c113ad89a9cf51a5aa0
Ralph Gasser <rg@pontius.ch>
2022-07-14 14:23:41 +0200

    Added a new endpoint to list column information.

---
+++ src/main/kotlin/org/vitrivr/cottontail/client/language/ddl/AboutColumn.kt
@@ -0,0 +1,47 @@
+package org.vitrivr.cottontail.client.language.ddl
+
+import org.vitrivr.cottontail.client.language.basics.LanguageFeature
+import org.vitrivr.cottontail.client.language.extensions.parseColumn
+import org.vitrivr.cottontail.grpc.CottontailGrpc
+
+/**
+ * A message to retrieves information about a column.
+ *
+ * @author Ralph Gasser
+ * @version 1.0.0
+ */
+class AboutColumn(name: String): LanguageFeature() {
+    /** Internal [CottontailGrpc.ListEntityMessage.Builder]. */
+    internal val builder = CottontailGrpc.ColumnDetailsMessage.newBuilder()
+
+    init {
+        this.builder.entity = name.parseColumn()
+    }
+
+    /**
+     * Sets the transaction ID for this [AboutColumn].
+     *
+     * @param txId The new transaction ID.
+     */
+    override fun txId(txId: Long): AboutColumn {
+        this.builder.metadataBuilder.transactionId = txId
+        return this
+    }
+
+    /**
+     * Sets the query ID for this [AboutColumn].
+     *
+     * @param queryId The new query ID.
+     */
+    override fun queryId(queryId: String): AboutColumn {
+        this.builder.metadataBuilder.queryId = queryId
+        return this
+    }
+
+    /**
+     * Returns the serialized message size in bytes of this [AboutColumn]
+     *
+     * @return The size in bytes of this [AboutColumn].
+     */
+    override fun serializedSize() = this.builder.build().serializedSize
+}
